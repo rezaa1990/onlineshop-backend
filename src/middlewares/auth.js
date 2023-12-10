4f3cd1754d09c030f20cfba59cdfca90498cb746
Reza <rezaaaa.z.1991@gmail.com>
2023-12-10 10:55:15 +0330

    login api updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,8 +5,9 @@ const debug = require('debug')("app:main");
 
 async function isLoggined(req,res,next){
   const token = req.header('token1')
+  debug("req:",req.header);
   debug("token00:",token)
-  if(!token) res.status(401).send('access denied');
+  if(!token) return res.status(401).send('توکن نامعتبر است');
   try{
     const decoded = jwt.verify(token, config.get("jwt_key"));
     const user = await User.findById(decoded._id).populate('basket');
@@ -14,13 +15,14 @@ async function isLoggined(req,res,next){
     req.user = user;
     next();
   }catch(ex){
-    res.status(400).send('invalid token');
+    res.status(400).send('ابتدا باید وارد سایت شوید');
   }
 }
 
 async function isAdmin(req,res,next){
-  if(!req.user.isadmin) res.status(403).send('access denied');
-  next();
+  if(req.user.role !== "adminUser"){
+    res.status(444).send('دسترسی امکان ندارد')
+  }else next();
 }
 
 module.exports = {
