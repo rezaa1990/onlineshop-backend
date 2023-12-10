c9f299ca75ed48a65614bfbe6fd77c508ec109b8
Reza <rezaaaa.z.1991@gmail.com>
2023-12-10 10:44:17 +0330

    make change to some coolection for save redundant collection

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 const debug = require('debug')("app:main");
 const config = require('config');
 
-module.exports = function(){
-  mongoose.set('strictQuery', false);
-  mongoose
-  .connect(config.get('db.address'))
-  .then(() => debug("connected to mongodb"))
-  .catch(() => debug("could not connect to mndb"));
-}
+module.exports = async function () {
+  try {
+    await mongoose.connect(config.get('db.address'));
+    console.log('Connected to MongoDB');
+
+    // حذف تمامی ایندکس‌های یکتایی برای مجموعه products
+    await mongoose.connection.db.collection('products').dropIndexes();
+    console.log('All unique indexes for products collection have been dropped');
+
+    // حذف تمامی ایندکس‌های یکتایی برای مجموعه users
+    await mongoose.connection.db.collection('users').dropIndexes();
+    console.log('All unique indexes for users collection have been dropped');
+
+    mongoose.set('strictQuery', false);
+    console.log('strictQuery set to false');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
