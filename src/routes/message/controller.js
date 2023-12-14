d5271a8b5745e339526edb37edd1d4e3e45b7b38
Reza <rezaaaa.z.1991@gmail.com>
2023-12-14 15:24:39 +0330

    message api updated

--- src/routes/message/controller.js
+++ src/routes/message/controller.js
@@ -25,52 +25,5 @@ module.exports = new (class extends controller {
     console.log(message);
     this.response({ res, message: "همه ی پیامها", code: 200, data: { message } });
   }
-  
-
-    // async updateProduct(req,res){
-    //   console.log(req.params.id);
-    //   console.log(req.body);
-    //   const result = await this.Product.findByIdAndUpdate(req.params.id, {
-    //     name: req.body.updateName,
-    //     price: req.body.updatePrice,
-    //     numberOfLike: req.body.updateNumberOfLikes,
-    //     description:req.body.updateDescription,
-    //     img:req.body.updateImg
-
-    //   })
-    //   this.response({ res, message: "مشخصات محصول بروزرسانی شد", code:200, data: { result } });
-    // };
-
-    // async addLike(req,res){
-    //   // console.log(req.params.id);
-    //   // console.log(req.body);
-    //   const product = await this.Product.findById(req.params.id);
-    //   if(!product) return this.response({res, message: "محصولی یافت نشد  ", code:404, data: {}});
-    //   if(product.numberOfLikes.includes(req.body.userId)) return (this.response({res, message:'این محصول قبلا توسط شما لایک شده است', code:400, data: {}}))
-    //   console.log('product.numberOfLikes',product.numberOfLikes);
-    //   const numberOfLike = [...product.numberOfLikes,req.body.userId];
-    //   product.numberOfLikes=numberOfLike;
-    //   await product.save();
-    //   this.response({ res, message: "محصول لایک شد", code:200, data: {product} });
-
-    // };
-
-    // async addComment(req,res){
-    //   const product = await this.Product.findById(req.body.productId);
-    //   if(!product) return this.response({res, message: "محصولی یافت نشد  ", code:404, data: {}});
-    //   product.comments = [...product.comments,req.body.commentId];
-    //   await product.save();
-    //   this.response({ res, message: "نظر شما ثبت شد", code:200, data: {product} });
-
-    // };
-
-    // async deleteProduct(req,res){
-    //   console.log(req.params.id);
-    //   console.log(req.body);
-    //   const product = await this.Product.findById(req.params.id);
-    //   if(!product) return this.response({res, message: "محصولی یافت نشد  ", code:404, data: {}})
-    //   const result = await this.Product.findByIdAndRemove(req.params.id);
-    //   this.response({ res, message: "محصول حذف گردید", code:200, data: { result } });
-    // };
 
 })();
--- src/routes/message/index.js
+++ src/routes/message/index.js
@@ -6,46 +6,16 @@ const {isLoggined , isAdmin} = require('./../../middlewares/auth')
 
 router.post(
   '/addmessage',
-  // validator.productsValidator(),
-  // controller.validate,
+  validator.messageValidator(),
+  controller.validate,
   controller.addMessage,
 );
 
 router.get(
   '/getmessages',
+  isLoggined,
+  isAdmin,
   controller.getMessages,
 );
-
-// router.put(
-//   '/updateproduct/:id',
-//   // isLoggined,
-//   // isAdmin,
-//   validator.productsValidator(),
-//   controller.validate,
-//   controller.updateProduct
-// );
-
-// router.put(
-//   '/addlike/:id',
-//   // isLoggined,
-//   // isAdmin,
-//   // validator.productsValidator(),
-//   controller.validate,
-//   controller.addLike
-// );
-
-// router.put(
-//   '/addcomment',
-//   // validator.productsValidator(),
-//   controller.validate,
-//   controller.addComment
-// );
-
-// router.delete(
-//   '/deleteproduct/:id',
-//   // isLoggined,
-//   // isAdmin,
-//   controller.deleteProduct
-// );
 
 module.exports = router;
