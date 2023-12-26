cf45a8cf1eee0ea22b3ad6d9097ac582bc95eabc
Reza <rezaaaa.z.1991@gmail.com>
2023-12-26 15:10:46 +0330

    some changes made

--- src/routes/user/controller.js
+++ src/routes/user/controller.js
@@ -21,16 +21,31 @@ module.exports = new (class extends controller {
     this.response({ res, message: "محصول به سبد شما افزوده شد", code:200, data: { user } });
   }
 
-  async removeFromBasket(req, res){
-    console.log('body:',req.body)
-    const user = await this.User.findById(req.body.userId);
-    const basketId = req.body.basketId;
-    console.log('basket1:',user.basket); 
-    user.basket=user.basket.filter(p => p.toString() !== basketId);
-    console.log('basket2:',user.basket);
-    await user.save();
-    this.response({ res, message: "محصول از سبد شما حذف شد", code:200, data: { user } });
+  async removeFromBasket(req, res) {
+    try {
+      const { userId, basketId } = req.body;
+  
+      const user = await this.User.findById(userId);
+      if (!user) {
+        return this.response({ res, message: 'کاربر پیدا نشد', code: 404 });
+      }
+      const basketid = await this.Product.findById(basketId);
+      if (!basketid) {
+        return this.response({ res, message: 'محصوول پیدا نشد', code: 404 });
+      }
+      const index = user.basket.indexOf(basketId);
+      console.log("index", index);
+      user.basket = user.basket.filter(p => p.toString() !== basketId);
+      user.numberOfEachProductInBasket = user.numberOfEachProductInBasket.filter((n,i)=>i !== index);
+  
+      await user.save();
+      this.response({ res, message: 'محصول از سبد شما حذف شد', code: 200, data: { user } });
+    } catch (error) {
+      console.error('Error removing from basket:', error);
+      this.response({ res, message: 'خطا در حذف محصول از سبد', code: 500 });
+    }
   }
+  
 
   async me(req, res){
     this.response({res,data:req.user})
